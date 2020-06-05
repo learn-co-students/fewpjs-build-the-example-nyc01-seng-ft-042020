7c5e5120f78908ea3909afe7fc03e46513355212
blobbyblobfish <aywang6@gmail.com>
2020-06-05 17:51:00 -0400

    Done.

--- main.js
+++ main.js
@@ -4,8 +4,46 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+document.querySelector("body").className = "hidden"
 
+document.addEventListener("DOMContentLoaded", () => {
 
+  const errorModal = document.querySelector("#modal")
+
+  function hideErrorModal() {
+    errorModal.className = "hidden"
+  }
+
+  function activateHeart(heart){
+    heart.textContent = FULL_HEART
+    heart.classList.add("activated-heart")
+  }
+
+  function deactivateHeart(heart) {
+    heart.textContent = EMPTY_HEART
+    heart.classList.remove("activated-heart")
+  }
+
+  document.addEventListener("click", (e) => {
+    if (e.target.classList.contains("activated-heart")) {
+      deactivateHeart(e.target)
+    }
+    else if (e.target.className === "like-glyph") {
+      mimicServerCall()
+      .then(activateHeart(e.target))
+      .catch(error => {
+        errorModal.classList.remove("hidden")
+        errorModal.innerText = error
+        setTimeout(hideErrorModal, 5000)
+      })
+
+    }
+
+  })
+
+  hideErrorModal()
+
+})
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
