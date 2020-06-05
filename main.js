d868f63821e407dc3fe58b8c34e07ee1d22a5aaf
Srishti Nath <srishti.nath@gmail.com>
2020-06-05 16:14:00 -0400

    Done.

--- main.js
+++ main.js
@@ -4,6 +4,30 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+// Change error to hidden
+
+const modal = document.getElementById('modal')
+modal.className = "hidden"
+
+document.addEventListener('click', function(e){
+  if (e.target.className == "like-glyph"){
+    mimicServerCall()
+    .then(response => {
+      e.target.className = "activated-heart"
+      e.target.innerText = FULL_HEART
+    })
+    .catch(response => {
+      modal.className.remove() 
+      modal.innerText = response
+      setTimeout((e) => {
+        modal.className = "hidden"
+      }, 5000)
+    })
+  } else if (e.target.className == "activated-heart") {
+    e.target.innerText = EMPTY_HEART
+    e.target.className = "like-glyph"
+  }
+})
 
 
 
